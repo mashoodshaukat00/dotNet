0b7f43f2292ccea62ea090a56459b41bbdde6550
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2024-11-12 17:36:51 +0100

    initiate variables for employee detail app

---
+++ EmployeeDetailApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int employeeId = 0;
+string firstName = string.Empty;
+string lastName = string.Empty;
+decimal annualSalary = 0;
+char gender = '\0';
+bool isManager = false;
+
+
+
