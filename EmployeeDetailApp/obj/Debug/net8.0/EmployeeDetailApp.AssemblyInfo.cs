b3385e8be3ba7057dc34233c28a203b682f0c0fa
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2024-11-21 16:04:33 +0100

    complete and test the app

--- EmployeeDetailApp/obj/Debug/net8.0/EmployeeDetailApp.AssemblyInfo.cs
+++ EmployeeDetailApp/obj/Debug/net8.0/EmployeeDetailApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeDetailApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0437ea87edc7f0b6c5ff6aca2ecd348369bb536b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b7f43f2292ccea62ea090a56459b41bbdde6550")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeDetailApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeDetailApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
